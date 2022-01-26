62bd825bd9892cf5387979bdf687f2e4067ac48a
kpg <kpg@posteo.net>
2022-01-26 13:40:25 +0000

    explicitly import from lib and organize utils
    FossilOrigin-Name: 5f655edceeb4fd05a8f5c3028abda37fea9012dc004e11f9559a63c89ccf38ed

--- nix-cl.nix
+++ nix-cl.nix
@@ -12,10 +12,71 @@
 
 { pkgs, lib, stdenv, ... }:
 
-with lib.lists;
-with lib.strings;
 
 let
+
+  inherit (lib)
+    length
+    filter
+    foldl
+    unique
+    id
+    concat
+    concatMap
+    findFirst
+    setAttr
+    getAttr
+    hasAttr
+    attrNames
+    attrValues
+    filterAttrs
+    mapAttrs
+    splitString
+    concatStringsSep
+    concatMapStringsSep
+    replaceStrings
+    optionalString
+    makeLibraryPath
+    makeSearchPath
+  ;
+
+  inherit (builtins)
+    head
+    tail
+    split
+    storeDir;
+
+  frequencies = xs:
+    let
+      getFreq = x: freqs:
+        if hasAttr x freqs
+        then getAttr x freqs
+        else 0;
+      lp = xs: freqs:
+        if builtins.length xs == 0
+        then freqs
+        else
+          let
+            x = toString (head xs);
+          in lp (tail xs) (setAttr freqs x (1 + (getFreq x freqs)));
+    in lp xs {};
+
+  zipmap = ks: vs:
+    let
+      lp = ks: vs: attrs:
+        if length ks == 0
+        then attrs
+        else lp (tail ks) (tail vs) (setAttr attrs (head ks) (head vs));
+    in
+      assert length ks == length vs;
+      lp ks vs {};
+
+  contains = xs: x:
+    if length xs == 0
+    then false
+    else if (head xs) == x
+    then true
+    else contains (tail xs) x;
 
   # Returns a flattened dependency tree without duplicates
   flattenedDeps = lispLibs:
@@ -268,49 +329,6 @@ let
         else pkg;
       pkg = substituteLib qlPkg;
     in pkg // { lispLibs = map substituteLib pkg.lispLibs; };
-
-  # FIXME move it out
-  setAttr = lib.setAttr;
-  getAttr = lib.getAttr;
-  hasAttr = lib.hasAttr;
-  attrNames = lib.attrNames;
-  attrValues = lib.attrValues;
-  filterAttrs = lib.filterAttrs;
-  mapAttrs = lib.mapAttrs;
-  concat = lib.concat;
-  id = lib.id;
-
-  frequencies = xs:
-    let
-      getFreq = x: freqs:
-        if hasAttr x freqs
-        then getAttr x freqs
-        else 0;
-      lp = xs: freqs:
-        if builtins.length xs == 0
-        then freqs
-        else
-          let
-            x = toString (head xs);
-          in lp (tail xs) (setAttr freqs x (1 + (getFreq x freqs)));
-    in lp xs {};
-
-  zipmap = ks: vs:
-    let
-      lp = ks: vs: attrs:
-        if length ks == 0
-        then attrs
-        else lp (tail ks) (tail vs) (setAttr attrs (head ks) (head vs));
-    in
-      assert length ks == length vs;
-      lp ks vs {};
-
-  contains = xs: x:
-    if length xs == 0
-    then false
-    else if (head xs) == x
-    then true
-    else contains (tail xs) x;
 
   # Creates a lisp wrapper with `packages` installed
   #
