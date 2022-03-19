88346a2b0a1c77d4f7d75fc9d8789fb7a9849eb8
kpg <kpg@posteo.net>
2022-03-19 21:36:44 +0000

    pin nixpkgs to avoid breakages resulting from new nixpkgs versions
    e.g. this commit breaks lispWithPackages by changing the way
    environment variables are prefixed using wrapProgram:
    
    https://github.com/NixOS/nixpkgs/commit/c67f885fe4cf70856b9f0052c1ce48f1abfcfe95
    
    FossilOrigin-Name: d3d34a6f51293be6731b06bd6f5e584670fcc3fe6fb587a1c8c92b6f24732317

--- default.nix
+++ default.nix
@@ -1,3 +1,10 @@
-{ pkgs ? import <nixpkgs> {}, ... }:
+{...}:
+
+let
+  pkgs = import (builtins.fetchTarball {
+    url = "https://github.com/nixos/nixpkgs/archive/21.11.tar.gz";
+    sha256 = "162dywda2dvfj1248afxc45kcrg83appjd0nmdb541hl7rnncf02";
+  }) {};
+in
 
 pkgs.callPackage ./nix-cl.nix {}
--- shell.nix
+++ shell.nix
@@ -1,6 +1,9 @@
 let
-  pkgs = import <nixpkgs> {};
-  nix-cl = import ./. { inherit pkgs; };
+  pkgs = import (builtins.fetchTarball {
+    url = "https://github.com/nixos/nixpkgs/archive/21.11.tar.gz";
+    sha256 = "162dywda2dvfj1248afxc45kcrg83appjd0nmdb541hl7rnncf02";
+  }) {};
+  nix-cl = import ./. {};
 in
 
 pkgs.mkShell {
