21347344ac23bcda1ef8f3a1c46be100b91276a1
kpg <kpg@posteo.net>
2022-04-22 21:00:45 +0000

    Use LTK fork that works with ABCL
    FossilOrigin-Name: 6cff9a3d758aca9755de4148867129a9f3cce16263af0711af8aa82274582a37

--- packages.nix
+++ packages.nix
@@ -344,6 +344,14 @@ let
     '';
   };
 
+  ltk = ql.ltk.overrideLispAttrs (o: {
+    src = builtins.fetchTarball {
+      url = "https://github.com/uthar/ltk/archive/f19162e76d6c7c2f51bd289b811d9ba20dd6555e.tar.gz";
+      sha256 = "0mzikv4abq9yqlj6dsji1wh34mjizr5prv6mvzzj29z1485fh1bj";
+    };
+    version = "f19162e76";
+  });
+
   nyxt = nyxt-gtk;
 
   s-sql_slash_tests = build-with-fix-duplicate-asds {
