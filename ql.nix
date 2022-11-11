d9f879c59ee5733eedb75226a54f6c80bc230500
Kasper Gałkowski <kpg@posteo.net>
2022-11-12 00:29:13 +0100

    fixed native dependency ql overlays

--- ql.nix
+++ ql.nix
@@ -1,165 +1,151 @@
 { pkgs, lib, build-asdf-system, ... }:
 
-with pkgs;
-with lib;
-with lib.lists;
-with lib.strings;
-
 let
 
   # FIXME: automatically add nativeLibs based on conditions signalled
 
-  extras = {
-    "cl+ssl" = pkg: {
-      nativeLibs = [ openssl ];
-    };
-    cl-cffi-gtk-glib = pkg: {
-      nativeLibs = [ glib ];
-    };
-    cl-cffi-gtk-cairo = pkg: {
-      nativeLibs = [ cairo ];
-    };
-    cl-cairo2 = pkg: {
-      nativeLibs = [ cairo ];
-    };
-    cl-cffi-gtk-gdk = pkg: {
-      nativeLibs = [ gtk3 ];
-    };
-    cl-cffi-gtk-gdk-pixbuf = pkg: {
-      nativeLibs = [ gdk-pixbuf ];
-    };
-    cl-cffi-gtk-pango = pkg: {
-      nativeLibs = [ pango ];
-    };
-    cl-gobject-introspection = pkg: {
-      nativeLibs = [ glib gobject-introspection ];
-    };
-    cl-mysql = pkg: {
-      nativeLibs = [ mysql-client ];
-    };
-    clsql-postgresql = pkg: {
-      nativeLibs = [ postgresql.lib ];
-    };
-    clsql-sqlite3 = pkg: {
-      nativeLibs = [ sqlite ];
-    };
-    cl-webkit2 = pkg: {
-      nativeLibs = [ webkitgtk ];
-    };
-    dbd-mysql = pkg: {
-      nativeLibs = [ mysql-client ];
-    };
-    lla = pkg: {
-      nativeLibs = [ openblas ];
-    };
-    cffi-libffi = pkg: {
-      nativeBuildInputs = [ libffi ];
-      nativeLibs = [ libffi ];
-    };
-    cl-rabbit = pkg: {
-      nativeBuildInputs = [ rabbitmq-c ];
-      nativeLibs = [ rabbitmq-c ];
-    };
-    trivial-ssh-libssh2 = pkg: {
-      nativeLibs = [ libssh2 ];
-    };
-    sqlite = pkg: {
-      nativeLibs = [ sqlite ];
-    };
-    cl-libuv = pkg: {
-      nativeBuildInputs = [ libuv ];
-      nativeLibs = [ libuv ];
-    };
-    cl-liballegro = pkg: {
+  overrides = (super: self: {
+    "cl+ssl" = self."cl+ssl".overrideLispAttrs (o: {
+      nativeLibs = [ pkgs.openssl ];
+    });
+    cl-cffi-gtk-glib = self.cl-cffi-gtk-glib.overrideLispAttrs (o: {
+      nativeLibs = [ pkgs.glib ];
+    });
+    cl-cffi-gtk-cairo = self.cl-cffi-gtk-cairo.overrideLispAttrs (o: {
+      nativeLibs = [ pkgs.cairo ];
+    });
+    cl-cairo2 = self.cl-cairo2.overrideLispAttrs (o: {
+      nativeLibs = [ pkgs.cairo ];
+    });
+    cl-cffi-gtk-gdk = self.cl-cffi-gtk-gdk.overrideLispAttrs (o: {
+      nativeLibs = [ pkgs.gtk3 ];
+    });
+    cl-cffi-gtk-gdk-pixbuf = self.cl-cffi-gtk-gdk-pixbuf.overrideLispAttrs (o: {
+      nativeLibs = [ pkgs.gdk-pixbuf ];
+    });
+    cl-cffi-gtk-pango = self.cl-cffi-gtk-pango.overrideLispAttrs (o: {
+      nativeLibs = [ pkgs.pango ];
+    });
+    cl-gobject-introspection = self.cl-gobject-introspection.overrideLispAttrs (o: {
+      nativeLibs = [ pkgs.glib pkgs.gobject-introspection ];
+    });
+    cl-mysql = self.cl-mysql.overrideLispAttrs (o: {
+      nativeLibs = [ pkgs.mysql-client ];
+    });
+    clsql-postgresql = self.clsql-postgresql.overrideLispAttrs (o: {
+      nativeLibs = [ pkgs.postgresql.lib ];
+    });
+    clsql-sqlite3 = self.clsql-sqlite3.overrideLispAttrs (o: {
+      nativeLibs = [ pkgs.sqlite ];
+    });
+    cl-webkit2 = self.cl-webkit2.overrideLispAttrs (o: {
+      nativeLibs = [ pkgs.webkitgtk ];
+    });
+    dbd-mysql = self.dbd-mysql.overrideLispAttrs (o: {
+      nativeLibs = [ pkgs.mysql-client ];
+    });
+    lla = self.lla.overrideLispAttrs (o: {
+      nativeLibs = [ pkgs.openblas ];
+    });
+    cffi-libffi = self.cffi-libffi.overrideLispAttrs (o: {
+      nativeBuildInputs = [ pkgs.libffi ];
+      nativeLibs = [ pkgs.libffi ];
+    });
+    cl-rabbit = self.cl-rabbit.overrideLispAttrs (o: {
+      nativeBuildInputs = [ pkgs.rabbitmq-c ];
+      nativeLibs = [ pkgs.rabbitmq-c ];
+    });
+    trivial-ssh-libssh2 = self.trivial-ssh-libssh2.overrideLispAttrs (o: {
+      nativeLibs = [ pkgs.libssh2 ];
+    });
+    sqlite = self.sqlite.overrideLispAttrs (o: {
+      nativeLibs = [ pkgs.sqlite ];
+    });
+    cl-libuv = self.cl-libuv.overrideLispAttrs (o: {
+      nativeBuildInputs = [ pkgs.libuv ];
+      nativeLibs = [ pkgs.libuv ];
+    });
+    cl-liballegro = self.cl-liballegro.overrideLispAttrs (o: {
       # build doesnt fail without this, but fails on runtime
       # weird...
-      nativeLibs = [ allegro5 ];
-    };
-    cl-ode = pkg: {
+      nativeLibs = [ pkgs.allegro5 ];
+    });
+    cl-ode = self.cl-ode.overrideLispAttrs (o: {
       nativeLibs = let
-        ode' = ode.overrideAttrs (o: {
+        ode' = pkgs.ode.overrideAttrs (o: {
           configureFlags = [
             "--enable-shared"
             "--enable-double-precision"
           ];
         });
       in [ ode' ];
-    };
-    classimp = pkg: {
-      nativeLibs = [ assimp ];
-    };
-    sdl2 = pkg: {
-      nativeLibs = [ SDL2 ];
-    };
-    lispbuilder-sdl-cffi = pkg: {
-      nativeLibs = [ SDL ];
-    };
-    cl-opengl = pkg: {
-      nativeLibs = [ libGL ];
-    };
-    cl-glu = pkg: {
-      nativeLibs = [ libGLU ];
-    };
-    cl-glut = pkg: {
-      nativeLibs = [ freeglut ];
-    };
-    cl-glfw = pkg: {
-      nativeLibs = [ glfw ];
-    };
-    cl-glfw-opengl-core = pkg: {
-      nativeLibs = [ libGL ];
-    };
-    cl-glfw3 = pkg: {
-      nativeLibs = [ glfw ];
-    };
-    lev = pkg: {
-      nativeLibs = [ libev ];
-    };
-    cl-rdkafka = pkg: {
-      nativeBuildInputs = [ rdkafka ];
-      nativeLibs = [ rdkafka ];
-    };
-    cl-async-ssl = pkg: {
-      nativeLibs = [ openssl ];
-    };
-    iolib = pkg: {
-      nativeBuildInputs = [ libfixposix ];
-      nativeLibs = [ libfixposix ];
+    });
+    classimp = self.classimp.overrideLispAttrs (o: {
+      nativeLibs = [ pkgs.assimp ];
+    });
+    sdl2 = self.sdl2.overrideLispAttrs (o: {
+      nativeLibs = [ pkgs.SDL2 ];
+    });
+    lispbuilder-sdl-cffi = self.lispbuilder-sdl-cffi.overrideLispAttrs (o: {
+      nativeLibs = [ pkgs.SDL ];
+    });
+    cl-opengl = self.cl-opengl.overrideLispAttrs (o: {
+      nativeLibs = [ pkgs.libGL ];
+    });
+    cl-glu = self.cl-glu.overrideLispAttrs (o: {
+      nativeLibs = [ pkgs.libGLU ];
+    });
+    cl-glut = self.cl-glut.overrideLispAttrs (o: {
+      nativeLibs = [ pkgs.freeglut ];
+    });
+    cl-glfw = self.cl-glfw.overrideLispAttrs (o: {
+      nativeLibs = [ pkgs.glfw ];
+    });
+    cl-glfw-opengl-core = self.cl-glfw-opengl-core.overrideLispAttrs (o: {
+      nativeLibs = [ pkgs.libGL ];
+    });
+    cl-glfw3 = self.cl-glfw3.overrideLispAttrs (o: {
+      nativeLibs = [ pkgs.glfw ];
+    });
+    lev = self.lev.overrideLispAttrs (o: {
+      nativeLibs = [ pkgs.libev ];
+    });
+    cl-rdkafka = self.cl-rdkafka.overrideLispAttrs (o: {
+      nativeBuildInputs = [ pkgs.rdkafka ];
+      nativeLibs = [ pkgs.rdkafka ];
+    });
+    cl-async-ssl = self.cl-async-ssl.overrideLispAttrs (o: {
+      nativeLibs = [ pkgs.openssl ];
+    });
+    iolib = self.iolib.overrideLispAttrs (o: {
+      nativeBuildInputs = [ pkgs.libfixposix ];
+      nativeLibs = [ pkgs.libfixposix ];
       systems = [ "iolib" "iolib/os" "iolib/pathnames" ];
-    };
-    "cl-ana.hdf-cffi" = pkg: {
-      nativeBuildInputs = [ hdf5 ];
-      nativeLibs = [ hdf5 ];
+    });
+    "cl-ana.hdf-cffi" = self."cl-ana.hdf-cffi".overrideLispAttrs (o: {
+      nativeBuildInputs = [ pkgs.hdf5 ];
+      nativeLibs = [ pkgs.hdf5 ];
       NIX_LDFLAGS = [ "-lhdf5" ];
-    };
-    gsll = pkg: {
-      nativeBuildInputs = [ gsl ];
-      nativeLibs = [ gsl ];
-    };
-    cl-libyaml = pkg: {
-      nativeLibs = [ libyaml ];
-    };
-    cl-libxml2 = pkg: {
-      nativeLibs = [ libxml2 ];
-    };
-    cl-readline = pkg: {
-      nativeLibs = [ readline ];
-    };
-  };
+    });
+    gsll = self.gsll.overrideLispAttrs (o: {
+      nativeBuildInputs = [ pkgs.gsl ];
+      nativeLibs = [ pkgs.gsl ];
+    });
+    cl-libyaml = self.cl-libyaml.overrideLispAttrs (o: {
+      nativeLibs = [ pkgs.libyaml ];
+    });
+    cl-libxml2 = self.cl-libxml2.overrideLispAttrs (o: {
+      nativeLibs = [ pkgs.libxml2 ];
+    });
+    cl-readline = self.cl-readline.overrideLispAttrs (o: {
+      nativeLibs = [ pkgs.readline ];
+    });
+  });
 
   qlpkgs =
     if builtins.pathExists ./imported.nix
     then pkgs.callPackage ./imported.nix { inherit build-asdf-system; }
     else {};
- 
-  builtQlpkgs = mapAttrs (n: v: build v) qlpkgs;
 
-  build = pkg:
-    let
-      withExtras = pkg //
-                   (optionalAttrs
-                     (hasAttr pkg.pname extras)
-                     (extras.${pkg.pname} pkg));
-     in build-asdf-system withExtras;
-
-in qlpkgs
+in qlpkgs.overrideScope' overrides
+# in qlpkgs
