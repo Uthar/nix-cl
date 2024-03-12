e2c2cc9d65d8f1a45e33a46a5ba6454ca4a3782b
Kasper Gałkowski <kpg@posteo.net>
2024-03-12 01:35:58 +0100

    update to quicklisp release 2023-10-21

--- import/main.lisp
+++ import/main.lisp
@@ -23,7 +23,7 @@
   (make-instance
    'quicklisp-repository
    :dist-url
-   "https://beta.quicklisp.org/dist/quicklisp/2023-02-15/"))
+   "https://beta.quicklisp.org/dist/quicklisp/2023-10-21/"))
 
 (defun run-importers ()
   (import-lisp-packages *quicklisp* *sqlite*)
