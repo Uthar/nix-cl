b5ebaee87f54bff140d0b0702bb078c7f364808e
Kasper Gałkowski <kpg@posteo.net>
2024-06-16 14:03:52 +0200

    adding scratch progress on nix profile changes etc

--- setup-hook.sh
+++ setup-hook.sh
@@ -1,56 +1,53 @@
-# This setup hook adds every propagated lisp system to CL_SOURCE_REGISTRY
 
-buildAsdfPath () {
-    declare -A seen=()
-    for dep in $propagatedBuildInputs; do
-        _addToAsdfPath $dep
-    done
-}
+# TODO: it might be faster to create files in temp conf dir in /build, then set
+# CL_SOURCE_REGISTRY to just (:import "/build/source-registry.conf.d")
+# ASDF_OUTPUT_TRANSLATIONS to just (:import "/build/output-translations.conf.d")
+#
+# How about nix-shell/dev shell? Is it fine to create files there, somewhere like /tmp?
 
-addFileToSearchPathWithCustomDelimiter() {
-    local delimiter="$1"
-    local varName="$2"
-    local file="$3"
-    if [[ -f "$file" && "${!varName:+${delimiter}${!varName}${delimiter}}" \
-          != *"${delimiter}${file}${delimiter}"* ]]; then
-        export "${varName}=${!varName:+${!varName}${delimiter}}${file}"
+addAsdfSourceRegistry () {
+  if test -z "${CL_SOURCE_REGISTRY:-}"; then
+    export CL_SOURCE_REGISTRY="(:source-registry :ignore-inherited-configuration)"
+  fi
+  if test -d "$1/share/common-lisp/source-registry.conf.d/"; then
+    if [[ ! "$CL_SOURCE_REGISTRY" =~ "$1/share/common-lisp/source-registry.conf.d/" ]]; then
+      export CL_SOURCE_REGISTRY="(:source-registry :ignore-inherited-configuration (:include \"$1/share/common-lisp/source-registry.conf.d/\")${CL_SOURCE_REGISTRY:49}"
     fi
+  fi
 }
 
-addFileToSearchPath() {
-    addFileToSearchPathWithCustomDelimiter ":" "$@"
-}
-
-_addToAsdfPath ()  {
-    local dep="$1"
-    if [ -v seen[$dep] ]; then
-        return
-    else
-        seen[$dep]=1
-        local path="$dep"
-
-        # FIXME slow
-
-        while read file; do
-            case "${file##*.}" in
-                jar) addFileToSearchPath "CLASSPATH" "$file" ;;
-                class) addToSearchPath "CLASSPATH" "${file%/*}" ;;
-                so) addToSearchPath "LD_LIBRARY_PATH" "${file%/*}" ;;
-                dylib) addToSearchPath "DYLD_LIBRARY_PATH" "${file%/*}" ;;
-                asd) addToSearchPath "CL_SOURCE_REGISTRY" "$path//" ;;
-            esac
-        done < <(find "$path" -type f,l -name '*.asd' -o -name '*.jar' \
-                      -o -name '*.class' -o -name '*.so' -o -name '*.dylib')
-
-        local prop="$dep/nix-support/propagated-build-inputs"
-
-        if [ -e "$prop" ]; then
-            local new_system
-            for new_system in $(cat $prop); do
-                _addToAsdfPath "$new_system"
-            done
-        fi
+addAsdfOutputTranslation () {
+  if test -z "${ASDF_OUTPUT_TRANSLATIONS:-}"; then
+    export ASDF_OUTPUT_TRANSLATIONS="(:output-translations :ignore-inherited-configuration)"
+  fi
+  if test -d "$1/share/common-lisp/asdf-output-translations.conf.d/"; then
+    if [[ ! "$ASDF_OUTPUT_TRANSLATIONS" =~ "$1/share/common-lisp/asdf-output-translations.conf.d/" ]]; then
+      export ASDF_OUTPUT_TRANSLATIONS="(:output-translations :ignore-inherited-configuration (:include \"$1/share/common-lisp/asdf-output-translations.conf.d/\")${ASDF_OUTPUT_TRANSLATIONS:53}"
     fi
+  fi
 }
 
-# addEnvHooks "$targetOffset" buildAsdfPath
+addLibToLibraryPath () {
+  if test -z "${LD_LIBRARY_PATH:-}"; then
+    export LD_LIBRARY_PATH=""
+  fi
+  if test -z "${DYLD_LIBRARY_PATH:-}"; then
+    export DYLD_LIBRARY_PATH=""
+  fi
+  if test -d "$1/lib"; then
+    if test -n "$(find "$1/lib" -name '*.so' -print -quit)"; then
+      if [[ ! "$LD_LIBRARY_PATH" =~ "$1/lib" ]]; then
+        export LD_LIBRARY_PATH="${LD_LIBRARY_PATH:+$LD_LIBRARY_PATH:}$1/lib"
+      fi
+    fi
+    if test -n "$(find "$1/lib" -name '*.dylib' -print -quit)"; then
+      if [[ ! "$DYLD_LIBRARY_PATH" =~ "$1/lib" ]]; then
+        export DYLD_LIBRARY_PATH="${DYLD_LIBRARY_PATH:+$DYLD_LIBRARY_PATH:}$1/lib"
+      fi
+    fi
+  fi
+}
+
+addEnvHooks "$hostOffset" addAsdfSourceRegistry
+addEnvHooks "$hostOffset" addAsdfOutputTranslation
+addEnvHooks "$hostOffset" addLibToLibraryPath
